bb1c8a181a43e35609f15869ffcc370492513afa
CIPHERTron <pritish.samal918@gmail.com>
2021-01-22 01:06:26 +0530

    Add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+  import firebase from 'firebase/app'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+ apiKey: "AIzaSyAf0JNoXyWRstX_tubjtmSYjXqEbxhRpBE",
+ authDomain: "project-osha.firebaseapp.com",
+ projectId: "project-osha",
+ storageBucket: "project-osha.appspot.com",
+ messagingSenderId: "1066816141159",
+ appId: "1:1066816141159:web:6feb73429e0d359dde21c2",
+ measurementId: "G-T3DF0D67DG"
+};
+
+export const Firebase = firebase.initializeApp(firebaseConfig);
